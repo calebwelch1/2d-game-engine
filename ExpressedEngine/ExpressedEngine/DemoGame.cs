fbf52603e781c9c7149f474b5d07a8db460057a0
calebwelch1 <calebjohn@posteo.net>
2023-12-16 15:31:09 -0500

    game window opens

---
+++ ExpressedEngine/ExpressedEngine/DemoGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressedEngine.ExpressedEngine;
+
+namespace ExpressedEngine
+{
+    class DemoGame : ExpressedEngine.ExpressedEngine
+    {
+        public DemoGame() : base(new Vector2(615, 515), "Expressed Engine Demo") { }
+    }
+}
---
+++ ExpressedEngine/ExpressedEngine/ExpressedEngine/ExpressedEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+    public abstract class ExpressedEngine
+    {
+        private Vector2 ScreenSize = new Vector2(512, 512);
+        private string Title = "New Game";
+        private Canvas Window = null;
+
+        public ExpressedEngine(Vector2 ScreenSize, string Title)
+        {
+            this.ScreenSize = ScreenSize;
+            this.Title = Title;
+
+            // creating window
+            // continuously call redraw whenever we can
+            // wan't meant to be a game window so difficult to redraw windows forms with many many game objects so we add this.DoubleBufferd
+
+            Window = new Canvas();
+            Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
+            Window.Text = this.Title;
+
+            Application.Run(Window);
+        }
+    }
+}
